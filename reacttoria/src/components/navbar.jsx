c724a786c804d1e56866cb6eb19b9d4d2e3ac396
connordevitt <connor.devitt2@gmail.com>
2025-04-30 10:15:23 -0400

    updates
    updates

--- reacttoria/src/components/navbar.jsx
+++ reacttoria/src/components/navbar.jsx
@@ -5,13 +5,11 @@ import { Link } from 'react-router-dom';
 const NavBar = () => {
   return (
     <Navbar bg="dark" variant="dark" expand="lg" className="fixed-top w-100">
-      <Navbar.Brand as={Link} to="/">Reacttoria</Navbar.Brand>
+      <Navbar.Brand as={Link} to="/">MLB Standings Tracker</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="ms-auto">
-          <Nav.Link as={Link} to="/">Home</Nav.Link>
+        <Nav className="ml-auto">
           <Nav.Link as={Link} to="/leaguestandings">League Standings</Nav.Link>
-          <Nav.Link as={Link} to="/about">About</Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
--- reacttoria/src/pages/landing.jsx
+++ reacttoria/src/pages/landing.jsx
@@ -16,13 +16,11 @@ const Landing = () => {
   return (
 
     <div className="bg-dark text-white min-vh-100 d-flex flex-column justify-content-between">
-      <header className="text-center py-5" data-aos="fade-down">
+      <header className="hero-section text-center py-5" data-aos="fade-down">
         <div className="container">
-          <h1 className="display-4 fw-bold">
-            Welcome to MLB Standings Tracker!
-          </h1>
-          <p className="lead text-muted">
-            Stay updated with the latest MLB standings all season long!
+          <h1 className="display-4 fw-bold">Welcome to MLB Standings Tracker</h1>
+          <p className="lead fw-bold w-100 mx-auto">
+            Stay up to date with the latest MLB standings and follow your favorite teams!
           </p>
           <Button
             variant="primary"
@@ -80,13 +78,22 @@ const Landing = () => {
       </main>
 
       <footer
-        className="bg-secondary py-3 mt-auto"
+        className="bg-dark py-3 mx-auto w-100" 
       >
-        <div id="footer-content" className="container text-center">
+        <div id="footer-content" className="container text-center sticky-footer">
           <small className="text-white">
             &copy; {new Date().getFullYear()} MLB Standings Tracker. All rights
             reserved.
           </small>
+          <p className="text-white mt-2 text-center">
+            Created by: {" "}
+            <a
+              href="https://github.com/connordevitt"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-white"
+            >Connor Devitt</a>
+          </p>
         </div>
       </footer>
     </div>
