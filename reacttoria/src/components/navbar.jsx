0455cd3bf509ceb8c71e49bb2a02ca7dc59a1e84
connordevitt <connor.devitt2@gmail.com>
2025-04-30 23:00:48 -0400

    Title changes & Mobile Nav fix.
    Reworked the nav bar to handle closing on mobile.

--- reacttoria/src/components/navbar.jsx
+++ reacttoria/src/components/navbar.jsx
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 const NavBar = () => {
-  return (
-    <Navbar bg="dark" variant="dark" expand="lg" className="fixed-top w-100">
-      <Navbar.Brand as={Link} to="/">MLB Standings Tracker</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  const [expanded, setExpanded] = useState(false);
+
+return(
+  <Navbar 
+      bg="dark" 
+      variant="dark" 
+      expand="lg" 
+      className="fixed-top w-100"
+      expanded={expanded}
+    >
+      <Navbar.Brand as={Link} to="/" onClick={() => setExpanded(false)}>
+        MLB Standings Tracker
+      </Navbar.Brand>
+      <Navbar.Toggle 
+        aria-controls="basic-navbar-nav" 
+        onClick={() => setExpanded(expanded ? false : true)}
+      />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ml-auto">
-          <Nav.Link as={Link} to="/leaguestandings">League Standings</Nav.Link>
+          <Nav.Link 
+            as={Link} 
+            to="/leaguestandings" 
+            onClick={() => setExpanded(false)}
+          >
+            League Standings
+          </Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
